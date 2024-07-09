07bd59f481f61eb4b2a9595eb5e916cc4ee3407d
Skwuruhl <smmarion11@gmail.com>
2024-07-08 18:23:55 -0600

    crosshair_scalar widget

--- better_crosshairs/scripts/mods/better_crosshairs/better_crosshairs_data.lua
+++ better_crosshairs/scripts/mods/better_crosshairs/better_crosshairs_data.lua
@@ -4,4 +4,15 @@ return {
 	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id      = "crosshair_scalar",
+				type            = "numeric",
+				default_value   = 1,
+				range           = {0.01, 8},
+				decimals_number = 2,
+			},
+		},
+	},
 }
--- better_crosshairs/scripts/mods/better_crosshairs/better_crosshairs_localization.lua
+++ better_crosshairs/scripts/mods/better_crosshairs/better_crosshairs_localization.lua
@@ -5,4 +5,10 @@ return {
 	mod_description = {
 		en = "Fixes crosshairs to be actually representative of spread, taking your FOV into account.",
 	},
+	crosshair_scalar = {
+		en = "Crosshair Scalar",
+	},
+	crosshair_scalar_description = {
+		en = "Multiplier to all crosshair sizes. Doesn't apply until next level load.\nCan be set with /set_crosshair_scalar",
+	},
 }
